2ee02b30276b3a1dd38c68083e6d12a6e75d80b3
John Davis <johnsdavis95@gmail.com>
2017-11-05 20:52:49 -0500

    set up most of file structure

---
+++ auction/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { RouterStore } from 'mobx-react-router';
+
+import './index.css';
+import App from './App';
+import store from './mobx';
+import registerServiceWorker from './registerServiceWorker';
+
+const routingStore = new RouterStore();
+const app = document.getElementById('root')
+
+ReactDOM.render(
+	<Provider
+		bidder={store.bidder}
+		bids={store.bids}
+	>
+		<App />
+	</Provider>, app);
+registerServiceWorker();
